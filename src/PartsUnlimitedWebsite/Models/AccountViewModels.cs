fbe3f0a122097d3fe9f5da515613216c410fb1c4
DANIEL LOPEZ <lcdanielf4@gmail.com>
2024-05-17 10:46:02 -0500

    El primer commit ok

--- src/PartsUnlimitedWebsite/Models/AccountViewModels.cs
+++ src/PartsUnlimitedWebsite/Models/AccountViewModels.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // Comentario de Daniel - rEMOTO
+// Comentario de Daniel - Local
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
